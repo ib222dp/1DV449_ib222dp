cdce79993d69bd8feedc107c3a617b0c194a67a7
ib222dp <ib222dp@student.lnu.se>
2015-11-04 10:14:49 +0100

    Second commit

--- Labb1/index.php
+++ Labb1/index.php
@@ -1,13 +1,11 @@
 <?php
 
-//curl_get_request("localhost:8080");
+curl_get_request("localhost:8080");
 
-/*function curl_get_request($url){
+function curl_get_request($url){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
 
     $data = curl_exec($ch);
     curl_close($ch); 
-}*/
-
-phpinfo();
+}
